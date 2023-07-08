1346ce3e99cba13ff9ba74e422f929d87d07436c
khodetsky <94411255+khodetsky@users.noreply.github.com>
2023-07-08 17:10:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/noname-digital-store/">
+    <BrowserRouter basename="/the-best-store/">
       <ThemeProvider theme={theme}>
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
